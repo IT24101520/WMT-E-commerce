cf94a75e32d29885bdbd0d086b0c93e8855a72a8
IT24101520 <it24101520@my.sliit.lk>
2026-05-02 16:38:15 +0530

    Update server.js

--- EGO_Project/backend/server.js
+++ EGO_Project/backend/server.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
     }
     next();
 });
-app.options('*', cors({ origin: true, credentials: true }));
+app.options('(.*)', cors({ origin: true, credentials: true }));
 app.use(express.json());
 
 // Routes
